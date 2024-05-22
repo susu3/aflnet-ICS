47eea8a23fd537644c4cbbe37ed545215236e5c5
meng_jy <mengjy@mail.ustc.edu.cn>
2024-05-22 10:32:52 +0800

    add modbus other request extract function

--- aflnet.c
+++ aflnet.c
@@ -737,7 +737,8 @@ typedef struct mbap_be{
 
 //buf: seed block; region_count_ref: region count
 //将buf分割为多个field，并指出每个field的大小，返回
-
+/*
+//按照每个field来划分
 region_t* extract_requests_modbus(unsigned char* buf, unsigned int buf_size, unsigned int* region_count_ref)
 {
   unsigned char* end_ptr = buf + buf_size -1;
@@ -810,9 +811,71 @@ region_t* extract_requests_modbus(unsigned char* buf, unsigned int buf_size, uns
   *region_count_ref = region_count; //region_count表示buf中一共有多少个field
   return regions; //将buf中每个field的开始结束位置标记
 }
-
+*/
 
 //按照每个request来划分
+region_t* extract_requests_modbus(unsigned char* buf, unsigned int buf_size, unsigned int* region_count_ref)
+{
+    unsigned char* end_ptr = buf + buf_size -1;
+    unsigned char* cur_ptr = buf;
+    unsigned int region_count = 0;
+    unsigned int cur = 0;
+    region_t* regions = NULL;
+
+    if (buf == NULL || buf_size == 0){
+        *region_count_ref = region_count;
+        return regions;
+    }
+
+    while (cur_ptr <= end_ptr){
+        unsigned int remaining_buf_size = end_ptr - cur_ptr + 1;
+        //MBAP + function code = 8 Bytes
+        if (remaining_buf_size >= sizeof(mbap_be)){
+            region_count ++;
+            regions = (region_t *)ck_realloc(regions, region_count*sizeof(region_t)); //Re-allocate a buffer, checking for issues and zeroing any newly-added tail
+
+            regions[region_count-1].state_sequence = NULL;
+            regions[region_count-1].state_count = 0;
+            regions[region_count-1].start_byte = cur;
+            //printf("region_count: %d; start_byte: %d",region_count,cur);
+
+            // check data region
+            mbap_be *header = (mbap_be *)cur_ptr;
+            // data field <= 252 bytes for valid packet
+            // length field = uid + fid + data <= 254
+            unsigned short remaining_packet_length = ushort_be_to_se(header->length);
+            remaining_packet_length = (remaining_packet_length > 254) ? 254 : remaining_packet_length;
+            unsigned short data_length = remaining_packet_length - 2;
+            unsigned int packet_length = remaining_packet_length + 6;
+            unsigned short available_data_length = (remaining_buf_size >= packet_length) ? data_length : remaining_buf_size - sizeof(mbap_be);
+
+            if (available_data_length > 0){
+                cur = cur + sizeof(mbap_be) + available_data_length -1;
+                regions[region_count -1].end_byte = cur++;
+                //printf("region_count: %d; end_byte: %d",region_count,cur);
+                cur_ptr = cur_ptr + sizeof(mbap_be) + available_data_length;
+            }else {
+                break;
+            }
+
+        }else {
+            //malformed
+            if (remaining_buf_size > 0){
+                region_count = region_count +1;
+                regions = (region_t *)ck_realloc(regions, region_count*sizeof(region_t));
+                regions[region_count-1].start_byte = cur;
+                regions[region_count-1].end_byte = cur + remaining_buf_size -1;
+                regions[region_count-1].state_sequence = NULL;
+                regions[region_count-1].state_count = 0;
+            }
+            break;
+        }
+    }
+
+    *region_count_ref = region_count; //region_count表示buf中一共有多少个field
+    return regions; //将buf中每个field的开始结束位置标记
+}
+
 
 
 
